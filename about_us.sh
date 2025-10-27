b052c9ecb7303e0c9765bbbece26b22d22cdb5dc
DARK ARIYAN <107464576+ANNOYMUSE@users.noreply.github.com>
2025-10-27 17:23:27 +0600

    Update about_us.sh with new author and channel info

--- about_us.sh
+++ about_us.sh
@@ -3,11 +3,11 @@ echo " "
 clear
 blue='\033[1;32m' 
 sleep 2
- echo "                      Coded BY >>>>> KEERTHAN >>>>>" |lolcat
+ echo "                      Coded BY >>>>> ARIYAN 2.0>>>>>" |lolcat
 sleep 1
-echo "                      YouTube Channel: Expert Hacker" |lolcat
+echo "                      YouTube Channel: ARIYAN 2.0 Hacker" |lolcat
 sleep 1
-echo -e "[ ✔ ] Follow Us on YouTube : https://www.youtube.com/channel/UC5GjaNLtyqJD8gkm7MI7f_A?view_as=subscriber"
+echo -e "[ ✔ ] Follow Us on YouTube : NO"
 echo -e "[ ✔ ] HAPPY HACKING ...DON'T FORGOT TO SUBSCRIBE"
 sleep 3
 bash main_script.sh
